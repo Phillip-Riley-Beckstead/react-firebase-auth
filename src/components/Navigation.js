fa1f047eae720a5901edb15d5fb2c5e9480d43aa
Unknown <rbeckstead_1991@hotmail.com>
2018-08-02 14:50:10 -0600

    full navigstion done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import * as routes from '../constants/routes';
+import { firebase } from '../firebase';
 
-const Navigation = () =>
-    <div>
-        <ul>
-            <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-            <li><Link to={routes.LANDING}>Landing</Link></li>
-            <li><Link to={routes.HOME}>Home</Link></li>
-            <li><Link to={routes.ACCOUNT}>Account</Link></li>
-        </ul>
-    </div>
+const withAuthentication = (Component) =>
+    class WithAuthentication extends React.Component {
+        constructor(props) {
+            super(props);
 
-export default Navigation;
+            this.state = {
+                authUser: null,
+            };
+        }
+
+        componentDidMount() {
+            firebase.auth.onAuthStateChanged(authUser => {
+                authUser
+                    ? this.setState({ authUser })
+                    : this.setState({ authUser: null });
+            });
+        }
+
+        render() {
+            return (
+                <Component />
+            );
+        }
+    }
+
+export default withAuthentication;
